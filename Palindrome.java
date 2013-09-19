0b4cdc60abfb513c111f97e1c5dcf9529448a702
Irene <irene@aurametrix.com>
2013-09-19 16:49:28 -0700

    debugging off-by-one error

--- Palindrome.java
+++ Palindrome.java
@@ -1,17 +1,22 @@
 package testpackage;
+
 public class Palindrome
 {
     public boolean test(String str)
     {
+		System.out.println ("You passed " + str.length() + "-length string: " + str);
         char givenstring[];
         char reverse[] = new char[str.length()];
         boolean flag = true;
         int count = 0, ctr = 0;
         givenstring = str.toCharArray();
+				System.out.println(givenstring);
         for(count = str.length() - 1; count >= 0; count++)
         {
             reverse[ctr] = givenstring[count];
+			System.out.println ("count & ctr " + count + " " + ctr + " reverse: " + reverse[ctr]);
             ctr++;
+			System.out.println ("count & ctr " + count + " " + ctr);
         }
         for(count = 0; count < str.length(); count++)
         {
